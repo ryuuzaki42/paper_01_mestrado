814f24a73d1bf0f7a2cede916328deb124ca9863
João Batista <joao42lbatista@gmail.com>
2016-12-19 18:46:01 -0200

    axes => axles

---
+++ changes.r
@@ -0,0 +1,9 @@
+= Changes in the paper =
+
+1 - Bologna scenario
+
+2 - Bus, private car (after taxi)
+    2.1  Bus route:
+        2.1.1 Air distance x Driving distance
+
+3 - Privacy of the driver and vehicle
